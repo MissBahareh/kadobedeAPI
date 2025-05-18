7a296c41beabab95ee18aa60a989ba9999c0f6e0
Bahareh <shiraz.bpn@gmail.com>
2025-05-18 18:42:12 +0330

    رفع مشکل json-server در Render

---
+++ index.js
@@ -0,0 +1,14 @@
+"";
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = 3100;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
